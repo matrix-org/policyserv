f5d99cec29739abdbfd7e54ed8c3d9842384b583
Travis Ralston <travisr@matrix.org>
2026-02-08 14:24:58 -0700

    Switch to using a slice of unsafe keys

--- filter/filter_unsafe_signing_key.go
+++ filter/filter_unsafe_signing_key.go
@@ -16,7 +16,7 @@ import (
 
 const UnsafeSigningKeyFilterName = "UnsafeSigningKeyFilter"
 
-var randZeroPrivateKey ed25519.PrivateKey
+var unsafePrivateSigningKeys = make([]ed25519.PrivateKey, 1)
 
 func init() {
 	mustRegister(UnsafeSigningKeyFilterName, &UnsafeSigningKeyFilter{})
@@ -26,7 +26,15 @@ func init() {
 	if err != nil {
 		panic(err) // "should never happen"
 	}
-	randZeroPrivateKey = priv
+	unsafePrivateSigningKeys[0] = priv
+}
+
+func UnsafeSigningKeys() []ed25519.PublicKey {
+	keys := make([]ed25519.PublicKey, len(unsafePrivateSigningKeys))
+	for i, priv := range unsafePrivateSigningKeys {
+		keys[i] = priv.Public().(ed25519.PublicKey)
+	}
+	return keys
 }
 
 type UnsafeSigningKeyFilter struct {
@@ -54,44 +62,46 @@ func (f *InstancedUnsafeSigningKeyFilter) CheckEvent(ctx context.Context, input 
 		return nil, fmt.Errorf("failed to redact event: %w", err)
 	}
 
-	// Sign the redacted event with our unsafe signing key
-	ownServerName := "__policyserv__"
-	ownKeyId := gomatrixserverlib.KeyID("ed25519:filter")
-	signed, err := gomatrixserverlib.SignJSON(ownServerName, ownKeyId, randZeroPrivateKey, redacted)
-	if err != nil {
-		return nil, fmt.Errorf("failed to sign event: %w", err)
-	}
+	// Sign the redacted event with our unsafe signing keys
+	for _, privKey := range unsafePrivateSigningKeys {
+		ownServerName := "__policyserv__"
+		ownKeyId := gomatrixserverlib.KeyID("ed25519:filter")
+		signed, err := gomatrixserverlib.SignJSON(ownServerName, ownKeyId, privKey, redacted)
+		if err != nil {
+			return nil, fmt.Errorf("failed to sign event: %w", err)
+		}
 
-	// Extract both the existing signatures and our new (unsafe) signature
-	var object struct {
-		Signatures map[string]map[gomatrixserverlib.KeyID]string // server name -> key ID -> signature (unpadded base64)
-	}
-	if err := json.Unmarshal(signed, &object); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal event: %w", err)
-	}
+		// Extract both the existing signatures and our new (unsafe) signature
+		var object struct {
+			Signatures map[string]map[gomatrixserverlib.KeyID]string // server name -> key ID -> signature (unpadded base64)
+		}
+		if err := json.Unmarshal(signed, &object); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal event: %w", err)
+		}
 
-	// Convert our signature to raw bytes. We don't compare base64 strings because some servers may (improperly)
-	// add padding, which throws off the check. Instead, we just compare the bytes.
-	compareSignature, err := base64.RawStdEncoding.DecodeString(object.Signatures[ownServerName][ownKeyId])
-	if err != nil {
-		return nil, fmt.Errorf("failed to decode own signature: %w", err)
-	}
-	delete(object.Signatures[ownServerName], ownKeyId) // remove our own signature so we don't generate a false positive
+		// Convert our signature to raw bytes. We don't compare base64 strings because some servers may (improperly)
+		// add padding, which throws off the check. Instead, we just compare the bytes.
+		compareSignature, err := base64.RawStdEncoding.DecodeString(object.Signatures[ownServerName][ownKeyId])
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode own signature: %w", err)
+		}
+		delete(object.Signatures[ownServerName], ownKeyId) // remove our own signature so we don't generate a false positive
 
-	// Compare the signatures
-	for serverName, keyIdToSignatures := range object.Signatures {
-		for keyId, signature := range keyIdToSignatures {
-			rawSignature, err := base64.RawStdEncoding.DecodeString(signature)
-			if err != nil {
-				return nil, fmt.Errorf("failed to decode signature (%s|%s): %w", serverName, keyId, err)
-			}
+		// Compare the signatures
+		for serverName, keyIdToSignatures := range object.Signatures {
+			for keyId, signature := range keyIdToSignatures {
+				rawSignature, err := base64.RawStdEncoding.DecodeString(signature)
+				if err != nil {
+					return nil, fmt.Errorf("failed to decode signature (%s|%s): %w", serverName, keyId, err)
+				}
 
-			if slices.Equal(compareSignature, rawSignature) {
-				log.Printf("[%s | %s] Unsafe signing key used by %s as key ID %s", input.Event.EventID(), input.Event.RoomID().String(), serverName, keyId)
-				return []classification.Classification{
-					classification.Spam,
-					classification.Unsafe,
-				}, nil
+				if slices.Equal(compareSignature, rawSignature) {
+					log.Printf("[%s | %s] Unsafe signing key used by %s as key ID %s", input.Event.EventID(), input.Event.RoomID().String(), serverName, keyId)
+					return []classification.Classification{
+						classification.Spam,
+						classification.Unsafe,
+					}, nil
+				}
 			}
 		}
 	}
