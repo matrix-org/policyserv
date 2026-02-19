3216a17a7bc85fdbad921e2e61d2ba014d5a9e18
Travis Ralston <travisr@matrix.org>
2026-02-18 17:17:51 -0700

    Fix flow control in prefilter channel receive
    First, it turns out the `break` statements were affecting the `select`, not the `for` loop. So, we add a flag to know that we're actually going to break from the infinite `for` loop.
    
    Second, it turns out that when receiving from a channel we can add a second parameter to capture whether the channel is still open. I'm not sure how much to trust this, so I've kept the `ClosingValue` stuff for now. The `stillOpen` flag will only be `false` with a zero value for `encoded` (an empty string, in our case).

--- filter/filter_hellban.go
+++ filter/filter_hellban.go
@@ -68,20 +68,26 @@ func newPrefilterHellban(set *Set, forTime time.Duration) (*InstancedHellbanFilt
 		},
 	}
 	go func(ch <-chan string, f *InstancedHellbanFilter) {
-		for {
+		keepLoop := true
+		for keepLoop {
 			select {
-			case encoded := <-ch:
+			case encoded, stillOpen := <-ch:
 				if encoded == pubsub.ClosingValue {
 					log.Println("Closing hellban cache listener")
-					break
+					keepLoop = false
+					break // `select`
 				}
 				if encoded == "" { // sometimes when closing we also get an empty string over the channel
-					continue
+					if !stillOpen {
+						keepLoop = false
+						break // `select`
+					}
+					continue // `for` loop
 				}
 
 				communityId, userId := mustDecodeHellban(encoded)
 				if communityId != f.set.communityId {
-					continue
+					continue // `for` loop
 				}
 
 				// Double check that the user isn't already hellbanned, otherwise we'll effectively permaban
