6334e9800ac4a09f4bd2e27b71cd6bd8982a651b
Travis Ralston <travisr@matrix.org>
2026-03-05 13:32:00 -0700

    Also delay between test cases

--- filter/foundation_test.go
+++ filter/foundation_test.go
@@ -31,7 +31,7 @@ func TestMatrixFoundationIntents(t *testing.T) {
 	communityConfig.SenderPrefilterAllowedSenders = &[]string{foundationAdmin}
 	communityConfig.MjolnirFilterEnabled = internal.Pointer(true)
 	communityConfig.DensityFilterMaxDensity = internal.Pointer(0.95)
-	communityConfig.HellbanPostfilterMinutes = internal.Pointer(5)
+	communityConfig.HellbanPostfilterMinutes = internal.Pointer(60) // large number to ensure the hellban will never expire in the test
 
 	cnf := &SetConfig{
 		CommunityId: "foundation",
@@ -78,6 +78,10 @@ func TestMatrixFoundationIntents(t *testing.T) {
 		} else {
 			assert.Greaterf(t, spamThreshold, vecs.GetVector(classification.Spam), "%d (%s) should NOT be spam, but was %f", i, run.Event.EventID(), vecs.GetVector(classification.Spam))
 		}
+
+		// Give things some time to settle before moving on to the next test case. This is primarily important for the
+		// hellban test cases to ensure the cause event creates a hellban before the effect event.
+		time.Sleep(100 * time.Millisecond)
 	}
 
 	// We deferred a bunch of close operations, but sometimes these operations can race with the test cases above causing
