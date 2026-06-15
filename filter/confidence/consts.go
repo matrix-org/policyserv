9c0f9901c6be19815af09038c727cf5220e02cb6
Travis Ralston <travisr@matrix.org>
2026-06-15 11:03:32 -0600

    Remove confidence and classifications structures

--- filter/confidence/consts.go
+++
@@ -1,4 +0,0 @@
-package confidence
-
-const UnlikelyButNotCertain float64 = 0.2 // arbitrarily set as "not quite 0.0"
-const LikelyButNotCertain float64 = 0.9   // arbitrarily set as "not quite 1.0"
